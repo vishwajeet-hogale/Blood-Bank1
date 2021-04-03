bcd7b0c23f779db2fb098b57544befd3c71b2ae7
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-04-03 21:26:24 +0530

    New changes

---
+++ README.md
@@ -0,0 +1,37 @@
+# Blood Bridge
+**The one stop solution connecting blood donors and organizations. https://blood-bridge.herokuapp.com/**  
+Blood Bridge eases the stress on the healthcare industry by making it easy for donors to donate and making sure that those who need blood can easily get it.  
+
+### Built for DSC PESIT Virtual Hack, April 4-5, 2020
+#### Theme: Healthcare
+
+## Features
+* Blood Bridge connects Prospective Donors with Blood Banks and Donation Camps in their area
+* Blood Bridge allows Blood banks and Hospitals to communicate supply and demand with each other
+* Blood Bridge lets Organizations put out Alerts for particular types of Blood urgently needed
+* Blood Bridge allows Organizations to set up Blood Donation Drives and make then visible to the public
+* Blood Bridge connects Organizations with Registered Donors
+
+## Donor's Application Usage https://blood-bridge.herokuapp.com/ 
+* You can register as a donor on the main page
+* You can also view blood banks and blood donation drives near you
+* You can view current urgent blood requirements
+
+## Organization's Application Usage https://blood-bridge.herokuapp.com/ 
+* Clicking on the Organization Dashboard tab in the top right will lead you to the Organization Dashboard.
+* Create a new Organization or login with email: test1@email.com password: test
+* The Organization dashboard displays: 
+  - Open Blood Requirement Alerts
+  - Current Blood Donation Drives
+  - Registered Donors
+* The dashboard also allows organizations to create new blood requirement alerts
+* New Blood Donation Drives can also be created using the dashboard
+
+## Technology Stack
+* Flask and Python used for the Backend
+* Website hosted on Heroku at https://blood-bridge.herokuapp.com/ 
+* Frontend written in HTML, JavaScript and CSS
+* Database used is MongoDB Atlas hosted on Google Cloud Platform (GCP) / Mumbai (asia-south1)  
+
+## The Idea
+There is a shortage of blood worldwide on normal days itself and during crisises like this current pandemic the shortage is even worse. My idea is to design a platform that allows donors to connect with Blood Banks and Hospitals which are in need of blood and allows Organizations dealing with blood to easily contact each other. Organizations can make their requirements public as to how much blood they need of which group and what type of blood they have in stock. Donors can contact these organizations and volunteer to donate the blood. Other organizations with a surplus of particular blood type can also help out each other. NGOs and Blood Banks hosting Blood Donation Camps can also make them public with the location so interested donors can donate easily.
--- app.py
+++ app.py
@@ -12,15 +12,118 @@ import random
 app = Flask(__name__)
 CORS(app)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.secret_key = "Dbadbakbkcb232e3jasbkabs7cabjbc"
+app.secret_key = b'\xd2(*K\xa0\xa8\x13]g\x1e9\x88\x10\xb0\xe0\xcc'
 
+#Loads the Database and Collections
+mongo = pymongo.MongoClient('mongodb+srv://srujan:mongodb@bloodbridge1-8kivy.gcp.mongodb.net/test?retryWrites=true&w=majority', maxPoolSize=50, connect=True)
+db = pymongo.database.Database(mongo, 'BloodBridge1')
+
+#Home page
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+	return render_template('index.html')
 
+#Map page
+@app.route('/map')
+def map_page():
+	return render_template('homepage.html')
+
+#Add new User
+@app.route('/add_new_user', methods=['POST'])
+def add_new_user():
+    inputData = dict(request.form)
+    Donor_Data = pymongo.collection.Collection(db, 'Donor_Data')
+    Donor_Data.insert_one(inputData)
+    return Response(status=200)
+
+#Org landing page
 @app.route('/org_lander')
 def org_lander():
     return render_template('orglander.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#Add new org
+@app.route('/add_new_org', methods=['POST'])
+def add_new_org():
+    Org_Data = pymongo.collection.Collection(db, 'Org_Data')
+    inputData = dict(request.form)
+    for i in json.loads(dumps(Org_Data.find())):
+        if i['email'] == inputData['email']:
+            return Response(status=304)
+    Org_Data.insert_one(inputData)
+    return Response(status=200)
+
+#Add new alert
+@app.route('/add_new_alert', methods=['POST'])
+def add_new_alert():
+	Alert_Data = pymongo.collection.Collection(db, 'Alert_Data')
+	inputData = dict(request.form)
+	Alert_Data.insert_one(inputData)
+	return Response(status=200)
+
+#Get alert data
+@app.route('/get_alert', methods=['POST'])
+def get_alert():
+	Alert_Data = pymongo.collection.Collection(db, 'Alert_Data')
+	data = json.loads(dumps(Alert_Data.find()))
+	data1 = {}
+	y = 0
+	data1['count'] = 0
+	for x in data:
+		data1["record"+str(y)] = x
+		y+=1
+	data1['count'] = y
+	return data1
+
+#Add new drive
+@app.route('/add_new_drive', methods=['POST'])
+def add_new_drive():
+	Drive_Data = pymongo.collection.Collection(db, 'Drive_Data')
+	inputData = dict(request.form)
+	Drive_Data.insert_one(inputData)
+	return Response(status=200)
+
+#Get drives
+@app.route('/get_drive', methods=['POST'])
+def get_drive():
+	Drive_Data = pymongo.collection.Collection(db, 'Drive_Data')
+	data = json.loads(dumps(Drive_Data.find()))
+	data1 = {}
+	y = 0
+	data1['count'] = 0
+	for x in data:
+		data1["record"+str(y)] = x
+		y+=1
+	data1['count'] = y
+	return data1
+
+#Get donors
+@app.route('/get_donors', methods=['POST'])
+def get_donors():
+	Donor_Data = pymongo.collection.Collection(db, 'Donor_Data')
+	data = json.loads(dumps(Donor_Data.find()))
+	data1 = {}
+	y = 0
+	data1['count'] = 0
+	for x in data:
+		data1["record"+str(y)] = x
+		y+=1
+	data1['count'] = y
+	return data1
+
+#Org login
+@app.route('/org_login', methods=['POST'])
+def org_login():
+	Org_Data = pymongo.collection.Collection(db, 'Org_Data')
+	inputData = dict(request.form)
+	for i in json.loads(dumps(Org_Data.find())):
+		if i['email'] == inputData['email'] and i['password'] == inputData['password']:
+			session['email'] = inputData['email']
+			return render_template('orgdashboard.html')
+	return Response(status=403)
+
+#Org landing page
+@app.route('/org_dash')
+def org_dash():
+    return render_template('orgdashboard.html')
+if __name__=="__main__":
+	app.run(debug=True)
